a237169e020f5757b87e3973511e79e34c8757ee
zhuhuilong <zhuhuilong@zto.cn>
2018-05-29 15:41:07 +0800

    格式首行缩进

--- webpack.common.js
+++ webpack.common.js
@@ -5,77 +5,77 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry: {
-        index: './src/index.js',
-        about: './src/about.js'
-    },
-    module: {
-        rules: [
+  entry: {
+    index: './src/index.js',
+    about: './src/about.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                minimize: true //css压缩
-                            }
-                        }
-                    ]
-                })
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    'csv-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
+              loader: 'css-loader',
+              options: {
+                minimize: true //css压缩
+              }
             }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'index'
+          ]
         })
-    ],
-    output: {
-        filename: "[name][hash].js",
-        chunkFilename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'index'
+    })
+  ],
+  output: {
+    filename: "[name][hash].js",
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  }
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,3 @@
-
-
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
